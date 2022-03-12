9424404148cfa928d58d7a7c6f78c05508331595
Haruhiko Takada <haruhiko.takada@gmail.com>
2022-03-12 20:56:56 +0900

    Timberの設定

--- android/app/src/main/kotlin/com/tfandkusu/try_platform_views/MainActivity.kt
+++ android/app/src/main/kotlin/com/tfandkusu/try_platform_views/MainActivity.kt
@@ -1,9 +1,19 @@
 package com.tfandkusu.try_platform_views
 
+import android.os.Bundle
+import io.flutter.BuildConfig
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
+import timber.log.Timber
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         flutterEngine
                 .platformViewsController
